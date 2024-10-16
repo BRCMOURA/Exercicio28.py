b2db919aa60d7a0b4ce249cff4bc5295a39448f4
BRCMOURA <brcmoura@gmail.com>
2024-10-16 19:49:22 -0300

    Add files via upload

---
+++ Exercício28.py
@@ -0,0 +1,16 @@
+#Peça o valor da compra e aplique um desconto de 5% se o valor for maior que R$ 100 e de 10% se for maior que R$ 500
+
+compra = float(input("Qual o valor da compra?"))
+
+if compra > 100 and compra <= 500:
+    desconto = compra * 0.05
+
+elif compra > 500:
+    desconto = compra * 0.10
+
+else:
+    desconto = 0
+
+total = compra - desconto
+
+print(f'O valor da compra com desconto é R$:{total:.2f}')
